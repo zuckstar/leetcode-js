089f667fc5045d62ed78991530b5a0306185a794
zhangmingle <15szhang2@gmail.com>
2021-03-25 22:03:07 +0800

    update: 数组+1

---
+++ 数组/简单-最小的k个数.js
@@ -0,0 +1,53 @@
+/*
+剑指 Offer 40. 最小的k个数
+输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+
+ 
+
+示例 1：
+
+输入：arr = [3,2,1], k = 2
+输出：[1,2] 或者 [2,1]
+示例 2：
+
+输入：arr = [0,1,2,1], k = 1
+输出：[0]
+ 
+
+限制：
+
+0 <= k <= arr.length <= 10000
+0 <= arr[i] <= 10000
+
+https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof/
+*/ 
+
+// 解法一：排序后取前 k 个数
+// 时间复杂度：O(n\log n)O(nlogn)，其中 nn 是数组 arr 的长度。算法的时间复杂度即排序的时间复杂度。
+ var getLeastNumbers = function(arr, k) {
+  arr.sort((a,b)=>{
+      return a-b
+  })
+  return arr.slice(0,k)
+};
+// 解法二：基于快排的 partition
+
+var getLeastNumbers = function(arr, k) {
+  const quickSort = (arr, k, l, r) => {
+      let i = l, j = r
+
+      while(i < j) {
+          while(i < j && arr[j] >= arr[l]) j--
+          while(i < j && arr[i] <= arr[l]) i++
+
+          [arr[i] ,arr[j]] = [arr[j], arr[i]] 
+      }
+      [arr[l], arr[i]] = [arr[i], arr[l]]
+
+      if(i > k) return quickSort(arr, k, l, i - 1)
+      if(i < k) return quickSort(arr, k, i + 1, r)
+      return arr.slice(0,k)
+  }
+  if(k >= arr.length) return arr
+  return quickSort(arr, k, 0, arr.length - 1)
+};
