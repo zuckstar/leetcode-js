a240f8e027de4ed2e42ced1f3718ef854204cb50
ZuckStar <527489516@qq.com>
2021-06-03 23:07:55 +0800

    feat: 接雨水

---
+++ 数组/002双指针/困难-接雨水.js
@@ -0,0 +1,77 @@
+/*
+42. 接雨水
+给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+
+示例 1：
+
+输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+输出：6
+解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。 
+示例 2：
+
+输入：height = [4,2,0,3,2,5]
+输出：9
+ 
+
+提示：
+
+n == height.length
+0 <= n <= 3 * 104
+0 <= height[i] <= 105
+
+https://leetcode-cn.com/problems/trapping-rain-water/
+*/
+
+
+// 动态规划法：
+var trap = function(height) {
+  const n = height.length;
+  if (n == 0) {
+      return 0;
+  }
+
+  const leftMax = new Array(n).fill(0);
+  leftMax[0] = height[0];
+  for (let i = 1; i < n; ++i) {
+      leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+  }
+
+  const rightMax = new Array(n).fill(0);
+  rightMax[n - 1] = height[n - 1];
+  for (let i = n - 2; i >= 0; --i) {
+      rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+  }
+
+  let ans = 0;
+  for (let i = 0; i < n; ++i) {
+      ans += Math.min(leftMax[i], rightMax[i]) - height[i];
+  }
+  return ans;
+};
+
+
+// 双指针法：
+var trap = function(height) {
+  let ans = 0
+
+  let leftMax = 0
+  let rightMax = 0
+
+  let left = 0
+  let right = height.length - 1
+
+  while(left < right) {
+      leftMax = Math.max(leftMax, height[left])
+      rightMax = Math.max(rightMax, height[right])
+
+      if(height[left] < height[right]) {
+          ans += leftMax - height[left]
+          left++
+      } else {
+          ans += rightMax - height[right]
+          right--
+      }
+  }
+
+  return ans
+};
